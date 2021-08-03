9aa259ca9d6084af7262c7e8fd113316b85e09a5
Jonathan Rainier <jonathan@rainier.tech>
2021-08-03 08:42:26 -0400

    Update Versions for Private Feed
    * Update version to upload to private feed

--- BitlyApiV4/BitlyBitlinksResponse.cs
+++ BitlyApiV4/BitlyBitlinksResponse.cs
@@ -1,12 +1,11 @@
 ﻿using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace BitlyAPI
 {
     public class BitlyBitlinksResponse
     {
         public BitlyPagination Pagination { get; set; }
-        
+
         public List<BitlyLink> Links { get; set; }
     }
 }
--- BitlyApiV4/BitlyDeepLink.cs
+++ BitlyApiV4/BitlyDeepLink.cs
@@ -1,25 +1,23 @@
-﻿using Newtonsoft.Json;
-
-namespace BitlyAPI
+﻿namespace BitlyAPI
 {
     public class BitlyDeepLink
     {
         public string Bitlink { get; set; }
-        
+
         public string InstallUrl { get; set; }
-        
+
         public string Created { get; set; }
-        
+
         public string AppUriPath { get; set; }
-        
+
         public string Modified { get; set; }
-        
+
         public string InstallType { get; set; }
-        
+
         public string AppGuid { get; set; }
-        
+
         public string Guid { get; set; }
-        
+
         public string Os { get; set; }
 
         public string BrandGuid { get; set; }
